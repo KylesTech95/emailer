deafe523cec9e58649d7e1001ca62008b0afbb7f
KylesTech95 <108553476+KylesTech95@users.noreply.github.com>
2024-04-21 16:35:58 -0400

    feat: arrows to focus

---
+++ public/fixarrows.js
@@ -0,0 +1,26 @@
+function fixarrows(arrows_arr, container){
+    console.log(arrows_arr)
+    console.log(container)
+    let container_obj = {
+        leftside:container.getBoundingClientRect().x,
+        rightside:container.getBoundingClientRect().x+container.clientWidth,
+    }
+    arrows_arr.forEach((arrow,index)=>{
+        // switch statement
+        switch(true){
+            // arrow - previous
+            case index%2!==0:
+                arrow.style.left = container_obj.leftside+'px';
+            break;
+            // arrow - next
+            case index%2===0:
+                arrow.style.left = (container_obj.rightside - (arrow.clientWidth-3))+'px';
+            break;
+            default:
+                console.log(undefined);
+            break;
+        }
+    })
+}
+
+export default fixarrows
--- public/main.js
+++ public/main.js
@@ -1,6 +1,36 @@
+import fixarrows from './fixarrows.js'
 import getEmail from './emailhelper.js'
 import screentimer from './screentimer.js'
+
+const fName = document.querySelector('#first')
+const lName = document.querySelector('#last')
+const name_con = document.getElementById('name-container')
+const arrows = document.querySelectorAll('.arrow')
 const secret = '/get-email'
 let to_field = document.getElementById('to')
-getEmail(secret,to_field)
-screentimer(window)
+const delayArrowsOnload = (arrow) => {
+    setTimeout(()=>arrow.classList.remove('arrow-opacity'),1750)
+}
+// getEmail(secret,to_field)
+// screentimer(window)
+fixarrows(arrows,name_con)
+
+arrows.forEach((arrow,index)=>{
+    delayArrowsOnload(arrow)
+    if(index==0){ // next arrow
+        arrow.addEventListener('click',e=>{
+            lName.focus();
+            e.target.classList.add('hide-arrow')
+            arrows[1].classList.remove('hide-arrow')
+            console.log('you went to last')
+        })
+    }
+    else{
+        arrow.addEventListener('click',e=>{
+            fName.focus();
+            e.target.classList.add('hide-arrow')
+            arrows[0].classList.remove('hide-arrow')
+            console.log('you went to first')
+        })
+    }
+})
