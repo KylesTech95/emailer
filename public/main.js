ed216a2e42f5f5ee4cbbc38a336cef97f0fcbb6f
KylesTech95 <108553476+KylesTech95@users.noreply.github.com>
2024-04-21 13:39:53 -0400

    feat: original session timer

--- public/main.js
+++ public/main.js
@@ -1,6 +1,6 @@
 import getEmail from './emailhelper.js'
+import screentimer from './screentimer.js'
 const secret = '/get-email'
-let to_field = document.getElementById('to-input')
+let to_field = document.getElementById('to')
 getEmail(secret,to_field)
-
-
+screentimer(window)
