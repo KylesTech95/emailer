a7ab15db1f964b9028e8e4ef447380196bc18f28
KylesTech95 <108553476+KylesTech95@users.noreply.github.com>
2024-04-22 19:01:26 -0400

    test: send 1 file with no incident

--- public/fetchfiles.js
+++ public/fetchfiles.js
@@ -11,33 +11,43 @@ const postFetch = async(action,data,method) => {
     })
     return response.json();
  }
+ const getFetch = (url) => {
+    let resp = fetch(url).then(res=>res.json()).then(data=>{
+        // console.log(data.sec)
+        return data.sec
+    })
+    return resp;
+ }
 
 function fetchFiles(form,filer,send){
+    const me_to = '/get-email'
     filer.addEventListener('click',e => {
         // e.preventDefault()
         const fileSystem = e.target.files
         console.log(fileSystem)
     })
     // attachments handler for images
-    const submitAttachments = (event) => {
+    const submitAttachments = async (event) => {
         event.preventDefault()
         let bod = {}
         const formdata = event.currentTarget;
         // const formBody = new FormData(formdata)
         const action = new URL(formdata.action).pathname
         const first = document.getElementById('first').value
+        const to = await getFetch(me_to) // fetch for mailbox name
         const last = document.getElementById('last').value
-        const email = document.getElementById('from').value
+        const from = document.getElementById('from').value
         const subject = document.getElementById('subject').value
         const message = document.getElementById('message').value
         const file = [...Object.values(filer.files)]
                             .map(file => {
-                                return {name:file.name,lastModified:file.lastModified,lastModifiedDate:file.lastModifiedDate,size:file.size,type:file.type}
+                                // return {name:file.name,lastModified:file.lastModified,lastModifiedDate:file.lastModifiedDate,size:file.size,type:file.type}
+                                return file.name
                             })
 
         // console.log(typeof files)
 
-        bod = {first,last,email,subject,message,file};
+        bod = {first,last,from,to,subject,message,file};
         const postOptions = {
             method:formdata.method,
             body: bod,
